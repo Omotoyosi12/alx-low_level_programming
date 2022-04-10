ad7181f7905d52741302a1a87fa7210794fc8ded
Omotoyosi <muhibahyusuf1204@gmail.com>
2022-04-10 07:02:43 -0700

    Toyosi

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,12 +4,12 @@
  * Description:  prints the alphabet in lowercase,
 * and then in uppercase, followed by a new line.
  * Return: 0
-*/
+ */
 int main(void)
 {
 char c = 'a';
 while (c <= 'z')
-}
+{
 putchar(c);
 c++;
 }
