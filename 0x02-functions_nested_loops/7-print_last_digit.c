7c70ca6ba09ee682951876095e33abb8bbf164a1
Omotoyosi <muhibahyusuf1204@gmail.com>
2022-04-14 00:56:08 -0700

    Toyosi

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,18 +1,18 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * print_last_digit - Ectry Function
- * @n: integer to get digit
- * Return: last digit
+ * print_last_digit - prints the last digit of a number
+ * @n:  is the int that will use for the argument of the function
+ * Return: integer value
  */
 int print_last_digit(int n)
 {
 int last;
+
 last = n % 10;
 if (last < 0)
-{
-last *= last * -1;
-}
+last = last * -1;
+
+
 _putchar(last + '0');
 return (last);
 }
